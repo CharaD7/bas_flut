14f4a6da30dcf37c59dc93d2645c0b1699139ddb
CharaD7 <jijakahn6@gmail.com>
2025-01-06 20:02:01 +0000

    Created basic textfield to take basic text and display on console screen

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -9,8 +9,39 @@ class ToDoPage extends StatefulWidget {
 }
 
 class _ToDoPageState extends State<ToDoPage> {
+  // text editing controller to get access to what the user typed
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // textfield
+              TextField(
+                controller: _textEditingController,
+                decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                  hintText: 'Enter your todo item',
+                ),
+              ),
+
+              // button
+              ElevatedButton(
+                onPressed: () {
+                  // print what the user typed
+                  print(_textEditingController.text);
+                },
+                child: const Text('Submit'),
+              )
+            ]
+          )
+        )
+      )
+    );
   }
 }
