ec2047b325ec75cc9b01b61fb90c12edcada8298
CharaD7 <jijakahn6@gmail.com>
2025-01-23 03:58:22 +0000

    Implemented use only padding and created toDoTile required variables (properties)

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  final String title;
-  final bool isChecked;
+  final String taskName;
+  final bool isCompleted;
   Function(bool?)? onChanged;
 
   TodoTile({
     super.key,
-    this.title = '',
-    this.isChecked = false,
-    this.onChanged,
+    required this.taskName,
+    required this.isCompleted,
+    required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only( left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: const EdgeInsets.all(15.0),
         decoration: BoxDecoration(
@@ -32,13 +32,13 @@ class TodoTile extends StatelessWidget {
           children: [
             // Checkbox
             Checkbox(
-              value: false,
-              onChanged: (bool? value) {},
+              value: isCompleted,
+              onChanged: onChanged,
             ),
 
             // Text
-            const Text(
-              'Buy milk',
+            Text(
+              taskName,
               style: TextStyle(
                 fontSize: 18.0,
                 decoration: TextDecoration.none,
