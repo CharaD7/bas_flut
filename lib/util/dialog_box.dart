32c80b5fbe258b1ea4c7c4e2b02186072881b05a
CharaD7 <jijakahn6@gmail.com>
2025-01-23 18:13:52 +0000

    Changed container to SizedBox, introduced container

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,20 +1,21 @@
 import 'package:bas_flut/util/u_button.dart';
-import 'package:bas_flut/util/u_button.dart';
 import 'package:flutter/material.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final controller;
+  const DialogBox({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.red[50],
-      content: Container(
+      content: SizedBox(
         height: 120,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextField(
+              controller: controller,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(6.0)),
