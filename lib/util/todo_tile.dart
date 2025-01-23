ee51b126d1c20057cf6b16a080fd111c4877419c
CharaD7 <jijakahn6@gmail.com>
2025-01-23 20:44:28 +0000

    Implemented slidable

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,47 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool isCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
     required this.taskName,
     required this.isCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only( left: 25.0, right: 25.0, top: 25.0),
-      child: Container(
-        padding: const EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              blurRadius: 5.0,
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade400,
             ),
           ],
         ),
-        child: Row(
-          children: [
-            Checkbox(
-              value: isCompleted,
-              onChanged: onChanged,
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                fontSize: 18.0,
-                decoration: isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5.0,
               ),
-            ),
-          ],
+            ],
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: isCompleted,
+                onChanged: onChanged,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  decoration: isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
