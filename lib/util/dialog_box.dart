f84b13f3fa6ee440865f7fa9bc6a678bcaec3d85
CharaD7 <jijakahn6@gmail.com>
2025-01-23 08:44:37 +0000

    Created DialogBox util/component

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.red[50],
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                  borderSide: BorderSide(color: Colors.red)
+                ),
+                hintText: "Add a new task",
+              )
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
