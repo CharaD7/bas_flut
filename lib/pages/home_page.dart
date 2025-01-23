6e2a1f1f1c1e627b2bd6a164a0fef97bb7c6f968
CharaD7 <jijakahn6@gmail.com>
2025-01-23 03:59:44 +0000

    Created list and method for toDo items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // List of Todo tasks
+  List toDoList = [
+    [ "Buy a tin of milk", false ],
+    [ "Record a tutorial", false ],
+  ];
+
+  // Checkbox was checked
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      // Update the state of the checkbox
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +34,17 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ListView(
-        children: const [
-          TodoTile(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            isCompleted: toDoList[index][1],
+            onChanged: (value) {
+              checkBoxChanged(value, index);
+            },
+          );
+        },
       ),
     );
   }
